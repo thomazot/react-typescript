55f8daba465dac3119b1b6115e99bf59040cf179
Thomaz T. O. Toyama <thomazot@gmail.com>
2020-01-02 20:03:26 -0300

    Missing file extension after upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,7 +45,18 @@ module.exports = {
         "import/prefer-default-export": "off",
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/explicit-member-accessibility": "off",
-        "@typescript-eslint/no-explicit-any": "off"
+        "@typescript-eslint/no-explicit-any": "off",
+        "import/extensions": [
+            "error",
+            "ignorePackages",
+            {
+                js: "never",
+                jsx: "never",
+                ts: "never",
+                tsx: "never",
+                mjs: "never"
+            }
+        ]
     },
     settings: {
         "import/parsers": {
